e9c4bca20b7cf5db91a0e68711cee3c3f17a91e4
Cecil Montemayor <cecilmontemayoriv@gmail.com>
2020-11-17 15:43:52 -0500

    added comments to exercise3.js

--- exercise3.js
+++ exercise3.js
@@ -1,21 +1,21 @@
 var FortuneTeller = require('galvanize-game-mechanics').FortuneTeller
 
-let seconedQuestion='Am I blue??'
+let seconedQuestion='Am I blue??' //simulating user input;
 
 function sometimeAsk (question=seconedQuestion) {
-    return FortuneTeller.ask(question)
+    return FortuneTeller.ask(question)  //Created function to send FortuneTeller user inputed question 
 }
 
 function alwaysAsk (question=`Will it be a good day?`) {
-    return FortuneTeller.ask(question)
+    return FortuneTeller.ask(question)  //This function will ALWAYS pass a question to FortuneTeller
 }
 
-sometimeAsk().then((resolve)=>{
-    console.log(resolve.response)
+sometimeAsk().then((resolve)=>{ //Attempt user inputed question, print FortuneTeller response if applicable
+    console.log(resolve.response)  
     
-}).catch((failure)=>{
+}).catch((failure)=>{ //If User Inputed question aka sometimeAsk Fails, then run the ALWAYS ask function
     alwaysAsk().catch((failure)=>{
-        console.log(failure.error);
+        console.log(failure.error); //If all else fails print error message;
     });
 });
 
